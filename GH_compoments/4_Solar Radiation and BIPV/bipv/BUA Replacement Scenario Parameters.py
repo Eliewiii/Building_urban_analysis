9227e2c576c560c9b31fcdabf1963b4971128879
Eliewiii <elie.medioniwiii@gmail.com>
2023-09-06 14:32:52 +0300

    add lca computatio, seperate transportation

---
+++ GH_compoments/4_Solar Radiation and BIPV/bipv/BUA Replacement Scenario Parameters.py
@@ -0,0 +1,17 @@
+"""Provides a scripting component.
+    Inputs:
+        x: The x script variable
+        y: The y script variable
+    Output:
+        a: The a output variable"""
+
+__author__ = "elie-medioni"
+__version__ = "2023.09.06"
+
+
+ghenv.Component.Name = "BUA Replacement Scenario Parameter"
+ghenv.Component.NickName = 'ReplacementScenarioParameter'
+ghenv.Component.Message = '0.0.0'
+ghenv.Component.Category = 'BUA'
+ghenv.Component.SubCategory = '4 :: Solar Radiation and BIPV'
+ghenv.Component.AdditionalHelpFromDocStrings = "1"
---
+++ GH_compoments/4_Solar Radiation and BIPV/bipv/BUA Run BIPV Simulation.py
@@ -0,0 +1,25 @@
+"""Generate mesh on buildings for BIPV simulation
+    Inputs:
+        path_simulation_folder_: Path to the folder. Default = Appdata\Local\Building_urban_analysis\Simulation_temp
+        building_id_list_: list of ints: list of buildings we want to run the simulation on
+        _path_weather_file: Path to the weather file. Default = todo
+        _north_: A number between -360 and 360 for the counterclockwise
+            difference between the North and the positive Y-axis in degrees.
+            90 is West and 270 is East. (Default: 0)
+        _overwrite_: bool: True if we want to rerun the simulation for buildings that were simulated before.
+                Otherwise they will be skipped and speed up the computation. (Default: False)
+    Output:
+        report: report
+        path_simulation_folder_: Path to the folder."""
+
+__author__ = "Eliewiii"
+__version__ = "2023.08.21"
+
+ghenv.Component.Name = "BUA Run BIPV Simulation"
+ghenv.Component.NickName = 'RunBIPVSimulation'
+ghenv.Component.Message = '0.0.0'
+ghenv.Component.Category = 'BUA'
+ghenv.Component.SubCategory = '4 :: BIPV And Solar Radiation'
+ghenv.Component.AdditionalHelpFromDocStrings = "1"
+
+
