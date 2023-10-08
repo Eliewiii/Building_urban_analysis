8d81d8af81fbee276486cf382456201c7d8c4f37
eliehome <elie.medioniwiii@gmail.com>
2023-10-08 16:17:09 +0300

    red shades from HB model read components

--- unit_tests/Elie_test_read_urban_canopy.py
+++ unit_tests/Elie_test_read_urban_canopy.py
@@ -2,9 +2,14 @@ from unit_tests.utils_main_import_scripts import *
 
 # Create simulation folder
 SimulationCommonMethods.make_simulation_folder()
+# # Create urban_canopy
+# urban_canopy_object = SimulationCommonMethods.create_or_load_urban_canopy_object(
+#     path_simulation_folder=default_path_simulation_folder)
+
 # Create urban_canopy
+path_simulation_folder = r'C:\Users\eliem\OneDrive - Technion\Ministry of Energy Research\IBPSA US conference\TA_Neighborhood_27.9.2023\Sim_8.10.23'
 urban_canopy_object = SimulationCommonMethods.create_or_load_urban_canopy_object(
-    path_simulation_folder=default_path_simulation_folder)
+    path_simulation_folder=path_simulation_folder)
 
 
 print("read")
