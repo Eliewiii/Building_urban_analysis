bbbec74afbdf2ab6ce77dfd4fa5cb94d54b6ab99
eliem <elie.medioniwiii@gmail.com>
2023-11-02 10:44:19 +0100

    new file for Simbuild paper simulation

--- unit_tests/Elie_test_read_urban_canopy.py
+++ unit_tests/Elie_test_read_urban_canopy.py
@@ -1,13 +1,13 @@
 from unit_tests.utils_main_import_scripts import *
 
 # Create simulation folder
-SimulationCommonMethods.make_simulation_folder()
+# SimulationCommonMethods.make_simulation_folder()
 # # Create urban_canopy
 # urban_canopy_object = SimulationCommonMethods.create_or_load_urban_canopy_object(
 #     path_simulation_folder=default_path_simulation_folder)
 
 # Create urban_canopy
-path_simulation_folder = r'C:\Users\eliem\OneDrive - Technion\Ministry of Energy Research\IBPSA US conference\TA_Neighborhood_27.9.2023\Sim_8.10.23'
+path_simulation_folder = r'C:\Users\eliem\Documents\Technion\Simulation_IBPSA\High_Density_Context'
 urban_canopy_object = SimulationCommonMethods.create_or_load_urban_canopy_object(
     path_simulation_folder=path_simulation_folder)
 
