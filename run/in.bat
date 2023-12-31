6c9a53e4100ebebb408c3972e6a823e5461e7784
eliem <elie.medioniwiii@gmail.com>
2023-12-31 14:22:18 +0100

    Load PolyFace3d working

--- run/in.bat
+++
@@ -1,3 +0,0 @@
-C:
-cd "C:\Users\alejandro.s\AppData\Local\Building_urban_analysis\Scripts\run"
-"C:\EnergyPlusV23-1-0\energyplus.exe" -w "C:\Users\alejandro.s\AppData\Local\Building_urban_analysis\Libraries\EPW\IS_5280_A_Haifa.epw" -r in.idf
