ec7eb55607d56464164a9a6aad16a1119b78e889
Eliewiii <elie.medioniwiii@gmail.com>
2024-06-09 16:36:30 +0300

    adjustment cscript context filter

--- unit_tests/Papers/Context_filter/Elie_context_filter_paper_UBES_simulation.py
+++ unit_tests/Papers/Context_filter/Elie_context_filter_paper_UBES_simulation.py
@@ -9,32 +9,34 @@ from copy import deepcopy
 
 from unit_tests.utils_main_import_scripts import *
 
+from building.building_modeled import BuildingModeled
+
 # Initialize logger
 dev_logger = logging.getLogger("dev")
-dev_logger.setLevel(logging.INFO)
-dev_handler = logging.FileHandler('../dev_log.log', mode='w')
+dev_logger.setLevel(logging.WARNING)
+dev_handler = logging.FileHandler('dev_log.log', mode='w')
 dev_formatter = logging.Formatter(
     '%(name)s: %(asctime)s - %(levelname)s - %(filename)s (function: %(funcName)s, line: %(lineno)d) - %(message)s')
 dev_handler.setFormatter(dev_formatter)
 dev_logger.addHandler(dev_handler)
 
 # Paths to the different files
-path_ubes_inputs = r"C:\Users\elie-medioni\OneDrive\OneDrive - Technion\Ministry of Energy Research\Papers\CheckContext\Simulations_Elie\Inputs\UBES"
-name_building_folder_list = ["Residential", "Office"]
+path_ubes_inputs = r"C:\Users\elie-medioni\OneDrive\OneDrive - Technion\Ministry of Energy Research\Papers\CheckContext\Simulations_Elie\Inputs\UBES_mini"  # todo : change to the correct path
+name_building_folder_list = ["Office"]  # ["Residential", "Office"]
 name_context_folder = "Context"
-name_context_alternative_list = ["context_1", "context_2", "context_3"]
-path_ep_simulation_parameter_json_file = None  # todo
+name_context_alternative_list = ["context_1", "context_2"]  #["context_1", "context_2", "context_3"]
+path_ep_simulation_parameter_json_file = default_path_hbjson_simulation_parameter_file
 
 # Initialize json result file
-name_result_json_file = "residential.json"
+name_result_json_file = "results_context_filter.json"
 path_result_folder = r"C:\Users\elie-medioni\OneDrive\OneDrive - Technion\Ministry of Energy Research\Papers\CheckContext\Simulations_Elie\results"
 path_json_result_file = os.path.join(path_result_folder, name_result_json_file)
 
 # EPW file
-path_epw = ""  # todo
+path_epw = r"C:\Users\elie-medioni\AppData\Local\Building_urban_analysis\Libraries\EPW\IS_5280_A_Tel_Aviv.epw"
 
 # Simulation parameters
-mvfc_list = [0.5, 0.5, 0.5]
+mvfc_list = [0.99, 0.1,0.05, 0.01]
 nb_of_rays = 3
 consider_windows = False
 
@@ -67,16 +69,16 @@ for name_building_folder in name_building_folder_list:
         # Create simulation folder
         SimulationCommonMethods.make_simulation_folder()
         # Make an original urban canopy object that will be copied for each simulation
-        urban_canopy_object_initial = SimulationCommonMethods.create_or_load_urban_canopy_object()
+        urban_canopy_object = SimulationCommonMethods.create_or_load_urban_canopy_object()
         # Load context from json
         SimulationLoadBuildingOrGeometry.add_buildings_from_lb_polyface3d_json_in_urban_canopy(
-            urban_canopy_object=urban_canopy_object_initial,
+            urban_canopy_object=urban_canopy_object,
             path_lb_polyface3d_json_file=path_context_alternative_json_file)
         # Make the merged face of the context buildings
         SimulationBuildingManipulationFunctions.make_merged_face_of_buildings_in_urban_canopy(
-            urban_canopy_object=urban_canopy_object_initial, overwrite=True)
+            urban_canopy_object=urban_canopy_object, overwrite=True)
         SimulationBuildingManipulationFunctions.make_oriented_bounding_boxes_of_buildings_in_urban_canopy(
-            urban_canopy_object=urban_canopy_object_initial, overwrite=True)
+            urban_canopy_object=urban_canopy_object, overwrite=True)
 
         # Load epw and simulation parameters
         UrbanBuildingEnergySimulationFunctions.load_epw_and_hb_simulation_parameters_for_ubes_in_urban_canopy(
@@ -87,11 +89,17 @@ for name_building_folder in name_building_folder_list:
         # Loop over the buildings
         for building_hbjson_name, building_hbjson_file in zip(building_hbjson_file_name_list,
                                                               building_hbjson_file_list):
+            # Delete previous traget building if there is any
+            target_building_id = None
+            for building_id, building_obj in urban_canopy_object.building_dict.items():
+                if isinstance(building_obj, BuildingModeled):
+                    target_building_id = building_id  # Get the first building that is a BuildingModeled, there is only one anyway
+                    break
+            if target_building_id is not None:
+                urban_canopy_object.remove_building_from_dict(target_building_id)
+
             # Clean name
             building_name = building_hbjson_name.split(".")[0]  # Remove the extension
-            # Copy the urban canopy object
-            urban_canopy_object = deepcopy(
-                urban_canopy_object_initial)  # If it does not work, can delete the BuildingModel objects
             # Load building from json
             SimulationLoadBuildingOrGeometry.add_buildings_from_hbjson_to_urban_canopy(
                 urban_canopy_object=urban_canopy_object,
@@ -99,6 +107,13 @@ for name_building_folder in name_building_folder_list:
                 path_file_hbjson=building_hbjson_file,
                 are_buildings_targets=True,
                 keep_context_from_hbjson=False)
+
+            # Get the id of the target building
+            target_building_id = None
+            for building_id, building_obj in urban_canopy_object.building_dict.items():
+                if isinstance(building_obj, BuildingModeled):
+                    target_building_id = building_id  # Get the first building that is a BuildingModeled, there is only one anyway
+                    break
 
             # Make the merged face of the building (not too sure if necessary)
             SimulationBuildingManipulationFunctions.make_merged_face_of_buildings_in_urban_canopy(
@@ -116,14 +131,18 @@ for name_building_folder in name_building_folder_list:
                     if alternative_identifier in json_result_dict:
                         continue
 
+                    # log
+                    dev_logger.warning(
+                        f"Building: {building_name}, Context alternative: {name_context_alternative}, MVFC: {mvfc}, No ray tracing: {no_ray_tracing}")
+
                     # Perform the context filtering
                     SimulationContextFiltering.perform_first_pass_of_context_filtering_on_buildings(
                         urban_canopy_object=urban_canopy_object,
-                        min_VF_criterion=mvfc,
+                        min_vf_criterion=mvfc,
                         overwrite=True)
                     SimulationContextFiltering.perform_second_pass_of_context_filtering_on_buildings(
                         urban_canopy_object=urban_canopy_object,
-                        nb_of_rays=nb_of_rays,
+                        number_of_rays=nb_of_rays,
                         consider_windows=consider_windows,
                         no_ray_tracing=no_ray_tracing,
                         keep_shades_from_user=True,
@@ -146,52 +165,52 @@ for name_building_folder in name_building_folder_list:
                         cop_heating=3., cop_cooling=3.)
 
                     # Extract all the results
-
-                    bes_duration = None
-                    bes_results_dict = {}
-
-
+                    context_selection_results_dict = urban_canopy_object.building_dict[
+                        target_building_id].shading_context_obj.to_dict()
+                    bes_results_dict = urban_canopy_object.building_dict[target_building_id].bes_obj.to_dict()
 
                     alternative_result_dict = {
                         "building_name": building_name,
                         "context_alternative": name_context_alternative,
                         "context_selection": {  # todo: fill the dictionary with the outputs
                             "first_pass": {
-                                "mvfc": None,
-                                "duration": None,
-                                "nb_selected_buildings": None,
+                                "mvfc": context_selection_results_dict["min_vf_criterion"],
+                                "duration": context_selection_results_dict["first_pass_duration"],
+                                "nb_selected_buildings": len(context_selection_results_dict["selected_context_building_id_list"]),
                             },
                             "second_pass": {
                                 "number_of_rays": nb_of_rays,
                                 "consider_windows": consider_windows,
-                                "duration": None,
-                                "nb_selected_shades": None,
+                                "no_ray_tracing": no_ray_tracing,
+                                "duration": context_selection_results_dict["second_pass_duration"],
+                                "nb_selected_shades": len(context_selection_results_dict["context_shading_hb_shade_list"]),
                                 "nb_user_shades": None,  # Louvers in that case
                             }
                         },
                         "BES":
                             {
-                                "cop_heating": None,
-                                "cop_cooling": None,
-                                "duration": None,
+                                "cop_heating": bes_results_dict["cop_heating"],
+                                "cop_cooling": bes_results_dict["cop_cooling"],
+                                "duration": bes_results_dict["sim_duration"],
                                 "results": {
-                                    "heating": bes_results_dict["heating"]["yearly"],
-                                    "cooling": bes_results_dict["cooling"]["yearly"],
-                                    "lighting": bes_results_dict["lighting"]["yearly"],
-                                    "equipment": bes_results_dict["equipment"]["yearly"],
-                                    "total": bes_results_dict["total"]["yearly"],
-                                    "h+c": bes_results_dict["heating"]["yearly"] + bes_results_dict["cooling"][
-                                        "yearly"],
-                                    "h+c+l": bes_results_dict["heating"]["yearly"] + bes_results_dict["cooling"][
-                                        "yearly"] + bes_results_dict["lighting"]["yearly"]
+                                    "heating": bes_results_dict["bes_results_dict"]["heating"]["yearly"],
+                                    "cooling": bes_results_dict["bes_results_dict"]["cooling"]["yearly"],
+                                    "lighting": bes_results_dict["bes_results_dict"]["lighting"]["yearly"],
+                                    "equipment": bes_results_dict["bes_results_dict"]["equipment"]["yearly"],
+                                    "total": bes_results_dict["bes_results_dict"]["total"]["yearly"],
+                                    "h+c": bes_results_dict["bes_results_dict"]["heating"]["yearly"] +
+                                           bes_results_dict["bes_results_dict"]["cooling"][
+                                               "yearly"],
+                                    "h+c+l": bes_results_dict["bes_results_dict"]["heating"]["yearly"] +
+                                             bes_results_dict["bes_results_dict"]["cooling"][
+                                                 "yearly"] + bes_results_dict["bes_results_dict"]["lighting"][
+                                                 "yearly"]
                                 }
                             }
                     }
 
-                # Update the result dictionary
-                if building_name not in json_result_dict:
-                    json_result_dict[building_name] = {}
-                json_result_dict[alternative_identifier] = alternative_result_dict
-                # Overwrite the json file
-                with open(path_json_result_file, 'w') as json_file:
-                    json.dump(json_result_dict, json_file)
+
+                    json_result_dict[alternative_identifier] = alternative_result_dict
+                    # Overwrite the json file
+                    with open(path_json_result_file, 'w') as json_file:
+                        json.dump(json_result_dict, json_file)
