4654f87557812d901d1ded0d2b9e3537dfc6b7ba
Eliewiii <elie.medioniwiii@gmail.com>
2024-07-08 13:08:57 +0300

    function for box charts duration

--- unit_tests/Papers/Context_filter/plot_context_selection_results/plot_various_graph_method.py
+++ unit_tests/Papers/Context_filter/plot_context_selection_results/plot_various_graph_method.py
@@ -75,12 +75,67 @@ def plot_bes_error_compared_to_ref_vs_mvfc(path_to_result_dict: str, building_ty
                         y_label="Error BES [%]", title=f"Error_compared_to_reference_{result_key}",
                         base_width=base_width, fig_height=fig_height, fig_width=fig_width)
 
+def plot_computation_time_compared_to_ref_vs_mvfc(path_to_result_dict: str, building_type="All", no_raytracing=False, base_width: float = 0.1,
+                                           fig_height: float = 6.,
+                                           fig_width: float = 9.):
+    """
+    Plot the error compared to the reference vs the MVFC. in ansolute value of the error
+    param path_to_result_dict: String. The path to the result dictionary.
+    param building_type: String. The building type to plot. "Residential", "Office" or "All"
+    param no_raytracing: Bool. If True, the raytracing results are not plotted.
+    param base_width: Float. The width of the boxes in the chart.
+    param fig_height: Float. The height of the figure.
+    param fig_width: Float. The width of the figure.
+    """
+    # Read the result dictionary
+    result_dict = read_json_dict(path_to_result_dict)
+    # Parse dictionary
+    parsed_dict = parse_result_dict(result_dict)
+    # Differentiate between raytracing and no raytracing
+    if no_raytracing:
+        raytracing_key = "no_ray_tracing"
+    else:
+        raytracing_key = "ray_tracing"
+    # Initialize data dictionary
+    data_dict = {}  # {MVFC: [errors]}
+
+    for sim_building_value in parsed_dict.values():
+        if building_type != "All" and sim_building_value["building_type"] != building_type:
+            continue
+        duration_reference_value = get_reference_value(sub_parsed_dict=sim_building_value,
+                                                  no_raytracing=no_raytracing, result_type="duration",
+                                                  result_key="BEM+Context_Selection")
+        for mvfc, result_dict in sim_building_value[raytracing_key].items():
+            if mvfc not in data_dict:
+                data_dict[mvfc] = []
+            BEM_duration = sim_building_value[raytracing_key][mvfc]["BES"]["duration"]
+            first_pass_duration = sim_building_value[raytracing_key][mvfc]["context_selection"]["first_pass"][
+                "duration"]
+            second_pass_duration = sim_building_value[raytracing_key][mvfc]["context_selection"]["second_pass"][
+                "duration"]
+            total_duration = BEM_duration + first_pass_duration + second_pass_duration
+            error = (total_duration / duration_reference_value-1) * 100
+            data_dict[mvfc].append(error)
+
+    # Sort the data to make sure there is no shift between x_postion and the data
+    x_positions = sorted(data_dict.keys())
+    data = [data_dict[x] for x in x_positions]
+
+    plot_xlog_box_chart(data=data, x_positions=x_positions, x_label="MVFC",
+                        y_label="Computation_time difference [%]", title=f"Computation_time_compared_to_reference",
+                        base_width=base_width, fig_height=fig_height, fig_width=fig_width)
+
 
 if __name__ == "__main__":
     path_to_result_dict = r"C:\Users\elie-medioni\OneDrive\OneDrive - Technion\Ministry of Energy Research\Papers\CheckContext\Simulations_Elie\results\Saved_results\results_context_filter_lca_lab_5.json"
-    plot_bes_error_compared_to_ref_vs_mvfc(path_to_result_dict,
-                                           building_type="Office",
-                                           no_raytracing=False,
-                                           result_key="h+c+l",
-                                           base_width=0.05,
-                                           fig_height=10., fig_width=15.)
+    # plot_bes_error_compared_to_ref_vs_mvfc(path_to_result_dict,
+    #                                        building_type="Residential",
+    #                                        no_raytracing=False,
+    #                                        result_key="h+c",
+    #                                        base_width=0.05,
+    #                                        fig_height=10., fig_width=15.)
+    plot_computation_time_compared_to_ref_vs_mvfc(path_to_result_dict,
+                                                  building_type="Office",
+                                                  no_raytracing=False,
+                                                  base_width=0.05,
+                                                  fig_height=10., fig_width=15.)
