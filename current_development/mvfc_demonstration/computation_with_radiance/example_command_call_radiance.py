038a9ffec058415659b7db869cc585d4bbbc8f39
Eliewiii <elie.medioniwiii@gmail.com>
2024-07-17 17:04:23 +0300

    radiance

--- current_development/mvfc_demonstration/computation_with_radiance/example_command_call_radiance.py
+++ current_development/mvfc_demonstration/computation_with_radiance/example_command_call_radiance.py
@@ -1,7 +1,7 @@
 import os
 
 path_radiance = r"D:\Elie\PhD\Software\Radiance\bin"
-path_rfluxmtx = r"C:\Program Files\ladybug_tools\radiance\bin\rfluxmtx.exe"
+path_rfluxmtx = r"D:\Elie\PhD\Software\Radiance\bin\rfluxmtx.exe"
 
 current_dir=r"C:\Users\elie-medioni\AppData\Local\Building_urban_analysis\Scripts\current_development\mvfc_demonstration\computation_with_radiance"
 
@@ -20,5 +20,6 @@ nb_ray = 100000
 #     f'CD "{path_radiance}" & "{path_rfluxmtx}" -h- -ab 0 -c {nb_ray} ' + f'"{path_emitter_rad_file}" "{path_receptor_rad_file}" > "{output_file}"')
 
 
-command = os.system(
-    f'rfluxmtx -h- -ab 0 -c {nb_ray} ' + f'"!xform -I "{path_emitter_rad_file}"" ' + f'"{path_emitter_rad_file}" "{path_receptor_rad_file}" > "{output_file}"')
+command = f'rfluxmtx.exe -h- -ab 0 -c {nb_ray} ' + f'"!xform -I "{path_emitter_rad_file}"" ' + f'"{path_emitter_rad_file}" "{path_receptor_rad_file}" > "{output_file}"'
+
+print(command)
