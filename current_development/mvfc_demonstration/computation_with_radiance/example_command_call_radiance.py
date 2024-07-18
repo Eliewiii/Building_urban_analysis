55e72a7d8f8035825d5ae767ab86937f99f6d860
Eliewiii <elie.medioniwiii@gmail.com>
2024-07-18 12:14:56 +0300

    radiance

--- current_development/mvfc_demonstration/computation_with_radiance/example_command_call_radiance.py
+++ current_development/mvfc_demonstration/computation_with_radiance/example_command_call_radiance.py
@@ -1,7 +1,7 @@
 import os
 
 path_radiance = r"D:\Elie\PhD\Software\Radiance\bin"
-path_rfluxmtx = r"D:\Elie\PhD\Software\Radiance\bin\rfluxmtx.exe"
+path_rfluxmtx = r"D:\Elie\PhD\Software\Radiance\bin\rfluxmtx"
 
 current_dir=r"C:\Users\elie-medioni\AppData\Local\Building_urban_analysis\Scripts\current_development\mvfc_demonstration\computation_with_radiance"
 
@@ -13,13 +13,20 @@ output_file = os.path.join(current_dir, "output.txt")
 
 nb_ray = 100000
 #
-# command = os.system(
-#     f'CD "{path_radiance}" & "{path_rfluxmtx}" -h- -ab 0 -c {nb_ray} ' + f'"!xform -I "{path_emitter_rad_file}"" ' + f'"{path_emitter_rad_file}" "{path_receptor_rad_file}" > "{output_file}"')
+
+
+command = f'rfluxmtx -h- -ab 0 -c {nb_ray} ' + f'"!xform -I "{path_emitter_rad_file}"" ' + f'"{path_receptor_rad_file}" > "{output_file}"'
+print(command)
+res = os.system(command)
+print (res)
+
+# command = f'CD "{path_radiance}" & "{path_rfluxmtx}" -h- -ab 0 -c {nb_ray} ' + f'"!xform -I "{path_emitter_rad_file}"" ' + f'"{path_emitter_rad_file}" "{path_receptor_rad_file}" > "{output_file}"'
+# print(command)
+# res = os.system(command)
 
 # command = os.system(
 #     f'CD "{path_radiance}" & "{path_rfluxmtx}" -h- -ab 0 -c {nb_ray} ' + f'"{path_emitter_rad_file}" "{path_receptor_rad_file}" > "{output_file}"')
 
 
-command = f'rfluxmtx.exe -h- -ab 0 -c {nb_ray} ' + f'"!xform -I "{path_emitter_rad_file}"" ' + f'"{path_emitter_rad_file}" "{path_receptor_rad_file}" > "{output_file}"'
+# command = f'rfluxmtx.exe -h- -ab 0 -c {nb_ray} ' + f'"!xform -I "{path_emitter_rad_file}"" ' + f'"{path_emitter_rad_file}" "{path_receptor_rad_file}" > "{output_file}"'
 
-print(command)
--- example_files/bipv_simulation/Elie_test_irradiance_simulation.py
+++ example_files/bipv_simulation/Elie_test_irradiance_simulation.py
@@ -8,7 +8,8 @@ urban_canopy_object = SimulationCommonMethods.create_or_load_urban_canopy_object
     path_simulation_folder=default_path_simulation_folder)
 
 # Run annual solar irradiance simulation
-SimFunSolarRadAndBipv.run_annual_solar_irradiance_simulation(urban_canopy_object=urban_canopy_object)
+SimFunSolarRadAndBipv.run_annual_solar_irradiance_simulation(urban_canopy_object=urban_canopy_object,
+                                                             overwrite=True)
 
 # Export urban_canopy to pickle
 SimulationCommonMethods.save_urban_canopy_object_to_pickle(urban_canopy_object=urban_canopy_object,
